e827844a33cc384d9227794b2a6c929c12761b5c
jumin <55091750+kjm541006@users.noreply.github.com>
2022-08-14 22:04:25 +0900

    Time: 94 ms (35.96%), Space: 42.3 MB (16.89%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.js
+++ 54-spiral-matrix/54-spiral-matrix.js
@@ -10,9 +10,7 @@ var spiralOrder = function(matrix) {
     let direction = 1;
     let xLen = matrix[0].length;
     let yLen = matrix.length;
-    console.log(xLen);
-    console.log(yLen);
-    let length = xLen* yLen;
+    let length = xLen * yLen;
     
     if(xLen == 1){
         return [...matrix];
@@ -21,34 +19,20 @@ var spiralOrder = function(matrix) {
     while(1){
         for(let i = 0; i < xLen; i++){
             x += direction;
-            console.log(matrix[y][x]);
             result.push(matrix[y][x]);
         }
         xLen --;
-        console.log(result);
         
         for(let j = 0; j < yLen - 1; j++){
             y += direction;
-            console.log(`y는 ${y}, x는 ${x}`);
-            console.log(matrix[y][x]);
-
             result.push(matrix[y][x]);
         }
         yLen --;
-        console.log(result);
-
         if(yLen == 0 || result.length == length) break;
-        
         direction *= -1;
-        console.log(result);
-
     }
-    console.log(result);
     return result;
 };
-
-
-
 // [00 01 02]
 // [10 11 12]
 // [20 21 22]
